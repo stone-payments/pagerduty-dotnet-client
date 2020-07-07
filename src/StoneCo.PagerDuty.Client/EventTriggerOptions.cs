f0b247a875ce6dbe7763d7e6b8f74206d99ee011
Daniel Reis <daniel.amorim@stone.com.br>
2020-07-07 13:29:28 -0300

    Adding EventTriggerOptions class

---
+++ src/StoneCo.PagerDuty.Client/EventTriggerOptions.cs
@@ -0,0 +1,19 @@
+﻿using StoneCo.PagerDuty.Client.Contracts;
+
+namespace StoneCo.PagerDuty.Client
+{
+    public class EventTriggerOptions
+    {
+        public EventTriggerOptions(Severity severity, string source, string summary)
+        {
+            Source = source;
+            Summary = summary;
+            Severity = severity;
+        }
+
+        public Severity Severity { get; }
+        public string Source { get; set; }
+        public string Summary { get; set; }
+        public string? DedupKey { get; set; }
+    }
+}
